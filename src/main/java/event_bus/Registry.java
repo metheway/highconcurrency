fdab0124b28daa1fe650c8081f48da5f514ed691
metheway <552418571@qq.com>
2021-06-09 19:22:20 +0800

    添加测试清空功能

--- src/main/java/event_bus/Registry.java
+++ src/main/java/event_bus/Registry.java
@@ -1,7 +1,43 @@
 package event_bus;
 
-public class Registry {
-    public void bind(Object subscriber) {
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class Registry {
+    // 线程安全的hashmap，保证多线程存储topic的安全。<topic, List<Subscriber>>
+    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<Subscriber>> subscriberContainer =
+            new ConcurrentHashMap<>();
+
+    // 队列主要保证顺序，并发性
+    public void bind(Object subscriber) {
+        List<Method> subscriberMethods = getSubscriberMethod(subscriber);
+        subscriberMethods.forEach(m -> tierSubscriber(subscriber, m));
+    }
+
+    private void tierSubscriber(Object subscriber, Method method) {
+        subscriberContainer.put("default-topic", new ConcurrentLinkedQueue<>());
+    }
+
+    private List<Method> getSubscriberMethod(Object subscriber) {
+        final List<Method> methods = new ArrayList<>();
+        Class<?> tmpClass = subscriber.getClass();
+        // 遍历类的父类，找到符合要求的方法Subscribe注解、参数1个、public标记
+        while (tmpClass != null) {
+            Method[] declairedMethod = tmpClass.getDeclaredMethods();
+            Arrays.stream(declairedMethod).filter(m -> m.isAnnotationPresent(Subscribe.class)
+                    && m.getParameterCount() == 1 && m.getModifiers() == Modifier.PUBLIC).
+                    forEach(methods::add);
+            tmpClass = tmpClass.getSuperclass();
+        }
+        return methods;
+    }
+
+    public void clear() {
+        this.subscriberContainer.clear();
     }
 }
--- src/main/test/event_bus/RegistryTest.java
+++ src/main/test/event_bus/RegistryTest.java
@@ -1,13 +1,107 @@
 package event_bus;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class RegistryTest {
-    @Test
-    public void testBind() {
-        Registry registry = new Registry();
-        registry.bind(null);
+import java.lang.reflect.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-        registry.bind(new Subscriber(null, null));
+public class RegistryTest {
+
+    // 验证bindcase，bind一个object，然后将方法等放入同topic的hashmap里面。
+    private final String caseTopic = "caseTopic";
+    private final TestSubscriber caseSubscriber = new TestSubscriber(null, null);
+
+    Registry registry = new Registry();
+    Method[] methods = registry.getClass().getDeclaredMethods();
+    Method getSubscriberMethod = null;
+    List<Method> methodList;
+    Object[] caseMethods;
+
+    @Before
+    public void setUp() throws Exception {
+        for (Method method : methods) {
+            if ("getSubscriberMethod".equals(method.getName())) {
+                getSubscriberMethod = method;
+            }
+        }
+        getSubscriberMethod.setAccessible(true);
+        methodList = (List<Method>) getSubscriberMethod.invoke(registry, caseSubscriber);
+        caseMethods = Arrays.stream(caseSubscriber.getClass().getDeclaredMethods()).filter(m ->
+                m.isAnnotationPresent(Subscribe.class) && m.getParameterCount() == 1 &&
+                        m.getModifiers() == Modifier.PUBLIC).toArray();
+    }
+
+    private class TestSubscriber extends Subscriber {
+
+        public TestSubscriber(Object subscribeObject, Method subscribeMethod) {
+            super(subscribeObject, subscribeMethod);
+        }
+
+        @Subscribe(topic = caseTopic)
+        public void firstMethod(int i) {
+            System.out.println("firstMethod: " + i);
+        }
+
+        @Subscribe(topic = caseTopic)
+        public void secMethod(int i) {
+            System.out.println("secMethod" + i);
+        }
+
+        public void thridMethod() {
+            System.out.println("thirdMethod");
+        }
+    }
+
+    @Test
+    public void testBind() throws NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Registry registry = new Registry();
+
+        // 绑定后验证并发的hashmap是否已经置入
+        registry.clear();
+        Field subscriberContainer = registry.getClass().getDeclaredField("subscriberContainer");
+        subscriberContainer.setAccessible(true);
+        Method sizeOfHashMap = subscriberContainer.getClass().getMethod("size");
+        Object sizeResult = sizeOfHashMap.invoke(subscriberContainer);
+
+        System.out.println(sizeResult);
+    }
+
+    @Test
+    public void testClear() throws NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Registry registry = new Registry();
+
+        // 注册一个吧
+//        registry.bind(new Subscriber(caseSubscriber, (Method) caseMethods[0]));
+        registry.bind(caseSubscriber);
+
+        Field subscriberContainer = registry.getClass().getDeclaredField("subscriberContainer");
+        subscriberContainer.setAccessible(true);
+
+        Method sizeOfMapMethod = subscriberContainer.getType().getMethod("size");
+        sizeOfMapMethod.setAccessible(true);
+
+        Object fieldValue = subscriberContainer.get(registry);
+        Integer length = (Integer) sizeOfMapMethod.invoke(fieldValue);
+        assert (length > 0);
+
+        // 调用clear后
+        Method clearMethod = registry.getClass().getMethod("clear");
+        clearMethod.invoke(registry);
+        fieldValue = subscriberContainer.get(registry);
+        length = (Integer) sizeOfMapMethod.invoke(fieldValue);
+        assert (length == 0);
+    }
+
+    @Test
+    public void testGetSubscriberMethod() throws InvocationTargetException, IllegalAccessException {
+        // 确认获取的方法返回值一样
+        assert (methodList.size() == caseMethods.length);
+        for (int i = 0; i < caseMethods.length; i++) {
+            assert (methodList.contains(caseMethods[i]));
+        }
     }
 }
