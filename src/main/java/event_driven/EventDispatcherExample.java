88f57f3d93f91b40928ae22e2e4e62523187a51e
metheway <552418571@qq.com>
2021-06-13 13:36:41 +0800

    填充框架内容

--- src/main/java/event_driven/EventDispatcherExample.java
+++ src/main/java/event_driven/EventDispatcherExample.java
@@ -4,27 +4,64 @@ public class EventDispatcherExample {
     public static void main(String[] args) {
         EventDispatcher eventDispatcher = new EventDispatcher();
         eventDispatcher.registerChannel(InputEvent.class, new InputEventHandler(eventDispatcher));
+        eventDispatcher.registerChannel(ResultEvent.class, new ResultEventHandler(eventDispatcher));
         eventDispatcher.dispatch(new InputEvent(1, 2));
     }
 
-    private static class InputEvent implements Message {
-        public InputEvent(int i, int j) {
+    private static class InputEvent extends Event {
+        private final int x;
+        private final int y;
 
+        public InputEvent(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
-        @Override
-        public Class<? extends Message> getType() {
-            return getClass();
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
         }
     }
 
-    private static class InputEventHandler implements Channel {
+    static class ResultEvent extends Event {
+        private final int result;
+
+        public ResultEvent(int result) {
+            this.result = result;
+        }
+
+        public int getResult() {
+            return result;
+        }
+    }
+
+    private static class InputEventHandler implements Channel<InputEvent> {
+        private final EventDispatcher eventDispatcher;
         public InputEventHandler(EventDispatcher eventDispatcher) {
+            this.eventDispatcher = eventDispatcher;
         }
 
         @Override
-        public void dispatch(Message message) {
+        public void dispatch(InputEvent message) {
+            System.out.println("x: " + message.getX() + ", y: " + message.getY());
+            int result = message.getX() + message.getY();
+            eventDispatcher.dispatch(new ResultEvent(result));
+        }
+    }
 
+    private static class ResultEventHandler implements Channel<ResultEvent> {
+        private final EventDispatcher eventDispatcher;
+
+        public ResultEventHandler(EventDispatcher eventDispatcher) {
+            this.eventDispatcher = eventDispatcher;
+        }
+
+        @Override
+        public void dispatch(ResultEvent message) {
+            System.out.println("the result is: " + message.getResult());
         }
     }
 }
